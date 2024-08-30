8ef83fba67a270f876c9f4ce58cd44d5d4051f61
龙门特产龙井茶 <tieyewang054@gmail.com>
2024-08-30 18:24:37 +0800

    Accomplish Zombie walk

--- PlantVSZombie/Assets/Script/Zombie.cs
+++ PlantVSZombie/Assets/Script/Zombie.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
+    private Rigidbody2D rgd;
+    public float moveSpeed = 0.7f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rgd = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rgd.MovePosition(rgd.position + Vector2.left * moveSpeed * Time.deltaTime);
     }
 }
